d2385c4edba410fc84bc3f227f60e1e8509c9c95
Igor Kodzhebash <igorkodzebash@gmail.com>
2022-04-08 17:59:19 +0300

    made charIndexOf function

--- script.js
+++ script.js
@@ -13,3 +13,19 @@ const isCharPresent = (str, char) => {
 
 let res1 = isCharPresent('Igot 1986?', '8');
 console.log(res1);
+
+
+const charIndexOf = (str, char) => {
+    for (let i = 0; i <= str.length - 1; i++) {
+        if (str[i] !== char) {
+            continue;
+        } else {
+            return i;
+        }
+
+    }
+    return -1;
+};
+
+let res2 = charIndexOf('La-la-la, this is?', ',');
+console.log(res2);
